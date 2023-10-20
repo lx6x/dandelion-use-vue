52fef093a9e3dbde3e7ff263b9f3a6dcff3bb7fd
lx6x <xiayong12321@163.com>
2023-10-20 22:18:10 +0800

    消息业务处理

---
+++ dandelion-server/dandelion-server-service/src/main/java/com/dandelion/use/server/service/notify/service/NotifyService.java
@@ -0,0 +1,10 @@
+package com.dandelion.use.server.service.notify.service;
+
+/**
+ * 消息业务处理
+ *
+ * @author lx6x
+ * @date 2023/10/20
+ */
+public interface NotifyService {
+}
---
+++ dandelion-server/dandelion-server-service/src/main/java/com/dandelion/use/server/service/notify/service/impl/NotifyServiceImpl.java
@@ -0,0 +1,14 @@
+package com.dandelion.use.server.service.notify.service.impl;
+
+import com.dandelion.use.server.service.notify.service.NotifyService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 消息业务处理实现
+ *
+ * @author lx6x
+ * @date 2023/10/20
+ */
+@Service
+public class NotifyServiceImpl implements NotifyService {
+}
